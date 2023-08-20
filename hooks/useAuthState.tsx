ac77eefa4a99b522030d016612e8ccb15cfcc8dd
yuktnk <yuki0wl@gmail.com>
2023-08-20 11:43:13 +0900

    Reduxセットアップ

---
+++ hooks/useAuthState.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebaseConfig';
+import { selectUser, login, logout } from '../slices/userSlice';
+
+export const useAuthState = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (authUser) => {
+      setIsLoading(true);
+
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            email: authUser.email!,
+          }),
+        );
+      } else {
+        dispatch(logout());
+      }
+      setIsLoading(false);
+    });
+
+    return () => {
+      unsub();
+    };
+  }, [user, isLoading]);
+};

1a9c3bec953d976447fa4b5c849d6de54c299d1b
yuktnk <yuki0wl@gmail.com>
2023-09-17 18:22:47 +0900

    ヘッダーにemail表示＋ログアウトボタン設置

--- navigations/TagStackNavigator.tsx
+++ navigations/TagStackNavigator.tsx
@@ -1,16 +1,54 @@
 import React, { FC } from 'react';
+import tw from 'tailwind-rn';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View, Alert } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { RootStackParamList } from '../types/types';
+import type { RootStackParamList } from '../types/types';
+
+import { auth } from '../firebaseConfig';
 import { TagListScreen } from '../screens/TagListScreen';
 import { CreateTagScreen } from '../screens/CreateTagScreen';
+import { IconButton } from '../components/IconButton';
+import { selectUser, logout } from '../slices/userSlice';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const TagStackNavigator: FC = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  const signOut = async () => {
+    try {
+      await auth.signOut();
+      dispatch(logout());
+    } catch {
+      Alert.alert('Logout error');
+    }
+  };
+
   return (
     <Stack.Navigator>
-      <Stack.Group>
+      <Stack.Group
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#008b8b',
+          },
+          headerTitle: user.email,
+          headerTintColor: 'white',
+          headerBackTitle: 'Back',
+          headerRight: () => (
+            <View style={tw('mr-3')}>
+              <IconButton
+                name="logout"
+                size={20}
+                color="white"
+                onPress={signOut}
+              />
+            </View>
+          ),
+        }}
+      >
         <Stack.Screen name="TagList" component={TagListScreen} />
       </Stack.Group>
       <Stack.Group
